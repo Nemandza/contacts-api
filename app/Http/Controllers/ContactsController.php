3f615baa344cc971772d7aff9985a780d9c655ba
BalogDani <balogdani@gmail.com>
2021-02-01 21:11:00 +0100

    Cntacts sa predavanja

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Contact;
+
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return response()->json($contacts);
+        
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $newContact = Contact::create($data);
+        return response()->json($newContact);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return response()->json($contact);
+    }
+
+   
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $contact = Contact::findOrFail($id);
+        $contact->update($data);
+        return response()->json($contact);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return response()->json($contact);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactsController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function(Request $request) {
+    return 'test radi';
+});
+
+Route::get('/contacts', [ContactsController::class, 'index']);
+
+Route::post('/contacts', [ContactsController::class, 'store']);
+Route::get('/contacts{id}', [ContactsController::class, 'show']);
+Route::put('/contacts{id}', [ContactsController::class, 'update']);
+Route::delete('/contacts{id}', [ContactsController::class, 'destroy']);
